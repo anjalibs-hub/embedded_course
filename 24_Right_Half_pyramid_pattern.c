c653d1a17955ed2064a2badcb2ea0277489d8f73
anjalibs-hub <anjalibs264@gmail.com>
2026-03-05 16:09:05 +0530

    Add files via upload

---
+++ 24_Right_Half_pyramid_pattern.c
@@ -0,0 +1,20 @@
+
+/*print Right pyramid of stars*/
+
+#include<stdio.h>
+int main()
+{
+    int row;
+    printf("Enter the nubmer of rows : ");
+    scanf("%d",&row);
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            printf("* ");
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
