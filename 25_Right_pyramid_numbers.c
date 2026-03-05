bc27fdfe4cb60454de09ed14ea22c51ba2ec7bb0
anjalibs-hub <anjalibs264@gmail.com>
2026-03-05 16:12:50 +0530

    Add files via upload

---
+++ 25_Right_pyramid_numbers.c
@@ -0,0 +1,20 @@
+
+/*print Right pyramid of numbers*/
+
+#include<stdio.h>
+int main()
+{
+    int row;
+    printf("Enter the nubmer of rows : ");
+    scanf("%d",&row);
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
